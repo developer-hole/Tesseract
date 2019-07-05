eb309b06b3ed7ad3c4b441f5f4830462465aed49
Piyush <bhangalepiyush@gmail.com>
2019-07-05 21:19:55 +0530

    Base Handler

---
+++ src/lib/structures/SetupClient.js
@@ -0,0 +1,108 @@
+const Discord = require('discord.js');
+const { extname, join, relative, sep } = require('path');
+const { isClass } = require('../utils');
+const fs = require('fs-nextra');
+
+class SetupClient extends Discord.Client {
+	/**
+	 * @typedef DatabaseOptions
+	 * @property {string} [uri="mongodb://localhost:27017/"] MongoDB URI.
+	 * @property {string} [name="setup_bot"] Name of the database.
+	 */
+
+	/**
+	 * @typedef ClientOptions
+	 * @property {DatabaseOptions} database Options for database.
+	 * @property {string} commandsDir The directory of commands.
+	 * @property {string} eventsDir The directory of events.
+	 */
+
+	/**
+	 * Initialize the Client.
+	 * @param  {ClientOptions} [options={}] - The options for new client.
+	 */
+
+	constructor(options = {}) {
+		if (options.constructor !== Object) {
+			throw new TypeError('The Clientoptions must be an Object.');
+		}
+
+		super(options);
+
+		// Directory Stuff
+		this.commandsDir = options.commandsDir || './commands';
+		this.eventsDir = options.eventsDir || './events';
+
+		// Command Stuff
+		this.commands = new Discord.Collection();
+		this.aliases = new Discord.Collection();
+
+		// Database huh.
+		this.database = {
+			name:
+				options.database && options.database.name
+					? options.database.name
+					: 'setup_bot',
+			uri:
+				options.database && options.database.uri
+					? options.database.uri
+					: 'mongodb://localhost:27017/'
+		};
+
+		this.init();
+	}
+
+	async load(directory, file) {
+		const loc = join(directory, ...file);
+		try {
+			const c = require(loc);
+			if (!isClass(c)) {
+				throw new TypeError(
+					`[Load Error] At ${loc} - Exported file is not a class.`
+				);
+			}
+			const command = new c();
+			this.commands.set(c.split('.')[0], command);
+			command.aliases.foreach(a => this.aliases.set(a, command));
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	async init() {
+		this.load_commands();
+		this.load_events();
+	}
+
+	async load_commands() {
+		const files = await fs
+			.scan(this.commandsDir, {
+				filter: (stats, path) => stats.isFile() && extname(path) === '.js'
+			})
+			.catch(() => fs.ensureDir(this.commandsDir))
+			.catch(e => console.error(e));
+		if (!files) return true;
+
+		return Promise.all(
+			[...files.keys()].map(file =>
+				this.load(__dirname, relative(__dirname, file).split(sep)))
+		);
+	}
+
+	async load_events() {
+		const files = await fs
+			.scan(this.eventsDir, {
+				filter: (stats, path) => stats.isFile() && extname(path) === '.js'
+			})
+			.catch(() => fs.ensureDir(this.eventsDir))
+			.catch(e => console.error(e));
+		if (!files) return true;
+
+		return Promise.all(
+			[...files.keys()].map(file =>
+				this.load(__dirname, relative(__dirname, file).split(sep)))
+		);
+	}
+}
+
+module.exports = SetupClient;
