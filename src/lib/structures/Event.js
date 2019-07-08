97869b6a2dd8b99bb1283bce89d7cda801af2b0c
Piyush <bhangalepiyush@gmail.com>
2019-07-08 13:57:59 +0530

    Update license

--- src/lib/structures/Event.js
+++ src/lib/structures/Event.js
@@ -1,19 +1,19 @@
 // Copyright (C) 2019 developer-hole
 //
-// This file is part of Server Setup.
+// This file is part of Tesseract.
 //
-// Server Setup is free software: you can redistribute it and/or modify
+// Tesseract is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// Server Setup is distributed in the hope that it will be useful,
+// Tesseract is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Server Setup.  If not, see <http://www.gnu.org/licenses/>.
+// along with Tesseract.  If not, see <http://www.gnu.org/licenses/>.
 
 class Event {
 	constructor(client, file, options = {}) {
