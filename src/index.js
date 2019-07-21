25b8a104084028e3cef5b741d829d1ad642f392c
Anish Shobith <anish.shobith19@gmail.com>
2019-07-21 22:41:57 +0530

    typings is here pg

--- src/index.js
+++ src/index.js
@@ -1,8 +1,44 @@
-const TesseractClient = require('./lib/structures/core/TesseractClient');
+// Examples
+
+// This is only for tests of the typings..
+
+// Client file
+const TesseractClient = require('./lib/structures/TesseractClient');
 
 new TesseractClient({
-	prefix: '-',
-	eventsDir: './events',
-	commandsDir: './commands',
-	owners: []
-}).login('TOKEN');
+	prefix: '',
+	eventsDir: '',
+	commandsDir: '',
+	owners: [],
+	database: { name: '', uri: '' }
+}).login();
+
+// Command File
+
+const Command = require('./lib/structures/Command');
+
+class command extends Command {
+	constructor(...args) {
+		super(...args, {
+			name: ''
+
+		});
+	}
+}
+
+module.exports = command;
+
+// Event file
+
+const Event = require('./lib/structures/Event');
+
+class event extends Event {
+	constructor(...args) {
+		super(...args, {
+			enabled: true,
+			once: false
+		});
+	}
+}
+
+module.export = event;
